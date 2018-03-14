c0f9a2aa67767f1cf150611abbcefc8d1c4d2d4c
ThirteenAG <thirteenag@gmail.com>
2018-03-14 21:33:43 +0300

    added timer for thread

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -9,12 +9,19 @@ DWORD WINAPI Init(LPVOID bDelay)
 
     if (pattern.count_hint(1).empty() && !bDelay)
     {
-        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&Init, (LPVOID)true, 0, NULL);
+        CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&Init, (LPVOID)true, 0, NULL));
         return 0;
     }
 
     if (bDelay)
-        while (pattern.clear().count_hint(1).empty()) { Sleep(0); };
+    {
+        HANDLE hTimer = NULL;
+        LARGE_INTEGER liDueTime;
+        liDueTime.QuadPart = -100000000LL;
+        hTimer = CreateWaitableTimer(NULL, TRUE, NULL);
+        SetWaitableTimer(hTimer, &liDueTime, 0, NULL, NULL, 0);
+        while (pattern.clear().count_hint(1).empty()) { Sleep(0); if (WaitForSingleObject(hTimer, INFINITE) == WAIT_OBJECT_0) { CloseHandle(hTimer);  return 0; } };
+    }
 
     injector::WriteMemory<int32_t>(pattern.get_first(5), INT_MAX, true);
     injector::WriteMemory<int32_t>(pattern.get_first(18), INT_MAX, true);
